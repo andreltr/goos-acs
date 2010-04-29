79fd0df2b3eff41614561b8e3be7e8645c85f6f9
rjofre <rjofre@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-04-29 12:46:22 +0000

    Archivos del proyecto.

---
+++ gDome/ACSdomeServer/include/acsdomeServerImpl.h
@@ -0,0 +1,29 @@
+#ifndef _DOME_SERVER_H
+#define _DOME_SERVER_H
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+#include <acscomponentImpl.h>
+#include <ACSErrTypeCommon.h>
+#include <acsdomeServerS.h>
+
+class DomeServer: public virtual acscomponent::ACSComponentImpl,   	//Component superclass
+		  public POA_acsdomeServer::DomeServer    					//CORBA servant stub
+{
+  public:
+	DomeServer(
+	       const ACE_CString& name,
+	       maci::ContainerServices * containerServices);
+
+    virtual ~DomeServer();
+
+    virtual void
+    displayMessage ();
+
+    virtual void
+    badMethod();
+};
+
+#endif
