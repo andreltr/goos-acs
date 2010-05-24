90e35b806bb9ef7464108e141e03424d1af75205
miguel.ortiz.cortes <miguel.ortiz.cortes@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-05-24 23:53:18 +0000

    creating some functions

--- gCCDBranch/gCCDComponent/src/STRSBIGST7.cpp
+++ gCCDBranch/gCCDComponent/src/STRSBIGST7.cpp
@@ -11,6 +11,8 @@ STRSBIGST7::STRSBIGST7() {
 }
 
 STRSBIGST7::~STRSBIGST7() {
+	delete p_cam;
+	delete p_Img;
 
 }
 
@@ -19,11 +21,27 @@ std::string* STRSBIGST7::getImage(int width, int height, int acquisitionMode,
 	std::cout << "STRSBIGST7::getImage()" << std::endl;
 
 	//Test queue
-	int queueSize = 4;
-	filesQueue = new std::string[queueSize];
-	filesQueue[0] = "inputImg00";
-	filesQueue[1] = "inputImg01";
-	filesQueue[2] = "inputImg02";
-	filesQueue[3] = "null";
-	return filesQueue;
+	/*int queueSize = 4;
+	 filesQueue = new std::string[queueSize];
+	 filesQueue[0] = "inputImg00";
+	 filesQueue[1] = "inputImg01";
+	 filesQueue[2] = "inputImg02";
+	 filesQueue[3] = "null";
+	 return filesQueue;*/
+
+}
+
+void STRLucaEMCCD::initialize() {
+	this->odp.deviceType = DEV_ETH;
+	this->odp.ipAddress = IP_ADDRESS;
+	p_Cam = new CSBIGCam(&odp);
+}
+
+void STRLucaEMCCD::shutDown() {
+	cout << "Closing 1st Device..." << endl;
+	if ((err = pCam->CloseDevice()) != CE_NO_ERROR)
+		break;
+	cout << "Closing 1st Driver..." << endl;
+	if ((err = pCam->CloseDriver()) != CE_NO_ERROR)
+		break;
 }
