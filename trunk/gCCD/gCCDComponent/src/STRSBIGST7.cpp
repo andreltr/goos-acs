033307fc4b00e6013eabe0ed2f5b9b8b8ae28ce6
miguel.ortiz.cortes <miguel.ortiz.cortes@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-06-05 18:01:32 +0000

    merged branches

--- trunk/gCCD/gCCDComponent/src/STRSBIGST7.cpp
+++ trunk/gCCD/gCCDComponent/src/STRSBIGST7.cpp
@@ -1,4 +1,5 @@
 #include "STRSBIGST7.h"
+#define IP_ADRESS	0xC0A8010C
 
 /*
  *
@@ -11,6 +12,8 @@ STRSBIGST7::STRSBIGST7() {
 }
 
 STRSBIGST7::~STRSBIGST7() {
+	delete p_Cam;
+	delete p_Img;
 
 }
 
@@ -18,12 +21,31 @@ std::string* STRSBIGST7::getImage(int width, int height, int acquisitionMode,
 		int numberOfAcquisitions, float exposureTime) {
 	std::cout << "STRSBIGST7::getImage()" << std::endl;
 
-	//Test queue
-	int queueSize = 4;
-	filesQueue = new std::string[queueSize];
-	filesQueue[0] = "inputImg00";
-	filesQueue[1] = "inputImg01";
-	filesQueue[2] = "inputImg02";
-	filesQueue[3] = "null";
-	return filesQueue;
+	initialize();
+	p_Img = new CSBIGImg;
+	p_Cam->GrabImage(p_Img, SBDF_LIGHT_ONLY);
+	p_Img->AutoBackgroundAndRange();
+	p_Img->SaveImage("img1.fits", SBIF_FITS);
+
+	std::string *filenames = new string[2];
+	filenames[0] = "img1";
+	filenames[1] = "null";
+
+	shutDown();
+
+	return filenames;
+}
+
+void STRSBIGST7::initialize() {
+	this->odp.deviceType = DEV_ETH;
+	this->odp.ipAddress = IP_ADRESS;
+	p_Cam = new CSBIGCam(odp);
+	p_Cam->EstablishLink();
+}
+
+void STRSBIGST7::shutDown() {
+	cout << "Closing 1st Device..." << endl;
+	p_Cam->CloseDevice();
+	cout << "Closing 1st Driver..." << endl;
+	p_Cam->CloseDriver();
 }
