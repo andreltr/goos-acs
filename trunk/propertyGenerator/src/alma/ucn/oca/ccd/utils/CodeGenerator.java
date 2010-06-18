9300534d8e24a109903d2099e9b2c9c95de7ea66
jackdurden02 <jackdurden02@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-06-18 01:47:25 +0000

    *Removed unused import

--- trunk/propertyGenerator/src/alma/ucn/oca/ccd/utils/CodeGenerator.java
+++ trunk/propertyGenerator/src/alma/ucn/oca/ccd/utils/CodeGenerator.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
