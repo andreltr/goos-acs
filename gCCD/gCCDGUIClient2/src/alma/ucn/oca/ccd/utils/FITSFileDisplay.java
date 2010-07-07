a31086212462939c67c7970e30f6dff12385004a
jackdurden02 <jackdurden02@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-07-07 00:18:53 +0000

    *Removed Jmagick code *Implemented some quick hacks to get image info *Fixed some bugs *Now you can open and save FITS files without connecting to a CCD

--- gCCD/gCCDGUIClient2/src/alma/ucn/oca/ccd/utils/FITSFileDisplay.java
+++ gCCD/gCCDGUIClient2/src/alma/ucn/oca/ccd/utils/FITSFileDisplay.java
@@ -4,170 +4,221 @@ import eap.fits.*;
 import eap.filter.*;
 
 import java.io.*;
+
 import javax.swing.*;
 
 import java.net.*;
 
 /*************************************************************************
-* Component for displaying a single FITS file.
-* This class makes up the core of both the standalone and applet versions
-* of the browser. You may embed this component in some other tool which 
-* needs to display or edit the contents of a FITS file.
-***************************************************************************/
+ * Component for displaying a single FITS file. This class makes up the core of
+ * both the standalone and applet versions of the browser. You may embed this
+ * component in some other tool which needs to display or edit the contents of a
+ * FITS file.
+ ***************************************************************************/
 public class FITSFileDisplay extends JTabbedPane {
 
-JMenuBar menu_bar;
-JFileChooser file_chooser;
-JTabbedPane tabs;
+	JMenuBar menu_bar;
+	JFileChooser file_chooser;
+	JTabbedPane tabs;
 
-FitsFile fits;
+	FitsFile fits;
 
-IOException background_exception;
+	IOException background_exception;
 
-FilterBroker filters;
+	FilterBroker filters;
 
-/****************************************************************************
-* Create a new display which does not display any file.
-****************************************************************************/
-public FITSFileDisplay()  {
+	// Misc
+	private int currentHDU;
+	private int totalHDU;
+	private double fileSize;
 
+	/****************************************************************************
+	 * Create a new display which does not display any file.
+	 ****************************************************************************/
+	public FITSFileDisplay() {
 
-    filters = new FilterBroker();
-    try { filters.addDefaultFilters(); }
-    catch(IOException e) {}
-    filters.setPasswordProvider(new PopupPasswordProvider(this));
+		filters = new FilterBroker();
+		try {
+			filters.addDefaultFilters();
+		} catch (IOException e) {
+		}
+		filters.setPasswordProvider(new PopupPasswordProvider(this));
 
+	} // end of constructor
 
-} // end of constructor
+	/**************************************************************************
+	 * Set the password provider to be used when reading an encrypted data
+	 * stream By default this class uses a
+	 * {@link eap.filter.PopupPasswordProvider}.
+	 **************************************************************************/
+	public void setPasswordProvider(PasswordProvider password) {
 
-/**************************************************************************
-* Set the password provider to be used when reading an encrypted data stream
-* By default this class uses a {@link eap.filter.PopupPasswordProvider}.
-**************************************************************************/
-public void setPasswordProvider(PasswordProvider password) {
+		filters.setPasswordProvider(password);
+	}
 
-    filters.setPasswordProvider(password);
-}
+	/**************************************************************************
+	 * Display the given file or URL.
+	 * 
+	 * @param string
+	 *            file name or URL to be displayed
+	 **************************************************************************/
+	public void load(String string) throws IOException {
 
-/**************************************************************************
-* Display the given file or URL.
-* @param string file name or URL to be displayed
-**************************************************************************/
-public void load(String string) throws IOException {
+		try {
+			load(new URL(string));
+			return;
+		} catch (MalformedURLException e) {
+		}
 
+		load(new File(string));
 
-    try { 
-        load(new URL(string)); 
-        return;
-    } catch(MalformedURLException e) {}
+	} // end of load String method
 
-    load(new File(string));
+	/**************************************************************************
+	 * Display the given file.
+	 * 
+	 * @param file
+	 *            The file to be displayed
+	 **************************************************************************/
+	public void load(File file) throws IOException {
+		fileSize = file.length();
+		if (filters.hasFilter(file.getName())) {
+			/*************************************************
+			 * we need to filter the data and that means using an InputStream as
+			 * the data source
+			 ************************************************/
+			InputStream in = new FileInputStream(file);
+			InputStream filtered = filters.filter(in, file.getName());
+			if (filtered != in) {
+				/***********************************
+				 * some filtering was actually done *
+				 ***********************************/
+				load(filtered);
+				return;
+			}
 
+		} // end if it looked like we should filter
 
-} // end of load String method
+		/***********************************************************
+		 * Either there wasn't any filtering to do, or the data didn't meet the
+		 * filters expectations. In either case we are reading the raw data, so
+		 * we might as well do that with a random access FITS file, since that's
+		 * better for FITS I/O
+		 ***********************************************************/
+		load(new RandomAccessFitsFile(new RandomAccessFile(file, "r")));
+	} // end of openFile method
 
-/**************************************************************************
-* Display the given file.
-* @param file The file to be displayed
-**************************************************************************/
-public void load(File file) throws IOException {
+	/**************************************************************************
+	 * Displays a URL
+	 * 
+	 * @param url
+	 *            the URL of a FITS file to display
+	 **************************************************************************/
+	public void load(URL url) throws IOException {
 
-    if(filters.hasFilter(file.getName()) ){
-        /*************************************************
-        * we need to filter the data and that
-        * means using an InputStream as the data source
-        ************************************************/
-        InputStream in = new FileInputStream(file);
-        InputStream filtered =filters.filter(in, file.getName());
-        if(filtered != in) {
-            /***********************************
-            * some filtering was actually done *
-            ***********************************/
-            load(filtered);
-            return;
-        }
+		/***********************************
+		 * open the URL, possibly filtering *
+		 ***********************************/
+		load(filters.filter(url.openStream(), url.getPath()));
 
-    } // end if it looked like we should filter
+	} // end of load URL method
 
+	/**************************************************************************
+	 * Display the FITS file which can be read from the given input stream.
+	 * 
+	 * @param in
+	 *            data stream from which to read FITS data.
+	 **************************************************************************/
+	public void load(InputStream in) throws IOException {
 
+		load(new InputStreamFitsFile(in));
 
-    /***********************************************************
-    * Either there wasn't any filtering to do, or the data
-    * didn't meet the filters expectations. In either case we
-    * are reading the raw data, so we might as well do that with a
-    * random access FITS file, since that's better for FITS I/O
-    ***********************************************************/
-    load( new RandomAccessFitsFile(
-          new RandomAccessFile(file,"r") ) );
+	} // end of load InputStream method
 
+	/*************************************************************************
+	 * Write the file currently displayed.
+	 * 
+	 * @param file
+	 *            The file to be written.
+	 *************************************************************************/
+	public void save(File file) throws IOException {
 
-} // end of openFile method
+		fits.write(new FileOutputStream(file));
 
-/**************************************************************************
-* Displays a URL
-* @param url the URL of a FITS file to display
-**************************************************************************/
-public void load(URL url) throws IOException {
+	} // end of save method
 
-    /***********************************
-    * open the URL, possibly filtering *
-    ***********************************/
-    load(filters.filter(url.openStream(), url.getPath()) );
+	/**************************************************************************
+	 * Display a FITS file which has previously been read by some other means.
+	 * 
+	 * @param fits
+	 *            The FITS file to display.
+	 **************************************************************************/
+	public void load(FitsFile fits) throws IOException {
 
-} // end of load URL method
+		this.fits = fits;
 
-/**************************************************************************
-* Display the FITS file which can be read from the given input stream.
-* @param in data stream from which to read FITS data.
-**************************************************************************/
-public void load(InputStream in) throws IOException {
+		/******************************
+		 * clear out any old FITS file *
+		 ******************************/
+		removeAll();
 
-    load(new InputStreamFitsFile(in));
+		/*********************
+		 * loop over all HDUs *
+		 *********************/
+		try {
+			totalHDU = 0;
+			for (int i = 0;; ++i) {
 
-} // end of load InputStream method
+				FitsHDU hdu = fits.getHDU(i);
+				FitsData data = hdu.getData();
+				FitsHeader header = hdu.getHeader();
 
-/*************************************************************************
-* Write the file currently displayed.
-* @param file The file to be written.
-*************************************************************************/
-public void save(File file) throws IOException {
+				++totalHDU;
 
-fits.write(new FileOutputStream(file));
+				add(i + ". " + header.getName() + " (" + header.getType() + ")",
+						new HDUDisplay(hdu));
+				// pack();
+				// validate();
+			}
+		} catch (NoSuchFitsHDUException e) {
+		}
 
-} // end of save method
+	} // end of displayFitsFile method
 
-/**************************************************************************
-* Display a FITS file which has previously been read by some other means.
-* @param fits The FITS file to display.
-**************************************************************************/
-public void load(FitsFile fits) throws IOException {
+	public double getImageWidth() {
+		try {
+			return fits.getHDU(currentHDU).getHeader().card("NAXIS1")
+					.doubleValue();
+		} catch (NoSuchFitsCardException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
 
-this.fits = fits;
+	public double getImageHeight() {
+		try {
+			return fits.getHDU(currentHDU).getHeader().card("NAXIS2")
+					.doubleValue();
+		} catch (NoSuchFitsCardException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
 
-/******************************
-* clear out any old FITS file *
-******************************/
-removeAll();
-
-/*********************
-* loop over all HDUs *
-*********************/
-try {
-    for(int i=0; ;++i) {
-
-        FitsHDU       hdu = fits.getHDU(i);
-		FitsData     data =  hdu.getData();
-        FitsHeader header =  hdu.getHeader();
-
-        add(i+". "+header.getName()+" ("+header.getType()+")",
-                 new HDUDisplay(hdu) );
- //       pack();
- //       validate();
-    }
-} catch(NoSuchFitsHDUException e) {}
-
-
-} // end of displayFitsFile method
-
+	public int getFrames() {
+		return totalHDU;
+	}
+	
+	public double getFileSize() {
+		return fileSize;
+	}
+	
+	public void setCurrentHDU(int currentHDU){
+		this.currentHDU = currentHDU;
+	}
 
 } // end of FITSFileDisplay class
