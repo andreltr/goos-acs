b63fca8a389fe65c549f4ecfed650b68da3aea14
guevarax <guevarax@d55a11f0-c6b6-1512-6fd8-a0a8490a29df>
2010-10-07 23:16:27 +0000

    Initial commit

---
+++ trunk/ACStelescope/include/acstelescopeServerImpl.h
@@ -0,0 +1,43 @@
+#ifndef _TELESCOPE_SERVER_H
+#define _TELESCOPE_SERVER_H
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+#include <ACSErrTypeCommon.h>
+#include <acstelescopeServerS.h>
+
+#include <baciCharacteristicComponentImpl.h>
+#include <baciRWdouble.h>
+#include <baciRWlong.h>
+
+#include <baciSmartPropertyPointer.h>
+
+class TelescopeServer: public baci::CharacteristicComponentImpl,
+			  public virtual POA_acstelescopeServer::TelescopeServer
+{
+  public:
+	TelescopeServer(
+	       const ACE_CString& name,
+	       maci::ContainerServices * containerServices);
+		   
+    virtual ~TelescopeServer();
+
+    virtual void displayMessage();
+    virtual void badMethod();
+    //virtual CORBA::Double getCurrentPosition() throw(CORBA::SystemException);
+
+    // ATRIBUTOS
+    virtual ACS::RWdouble_ptr
+    currentPositionAlpha ();
+
+    virtual ACS::RWdouble_ptr
+    currentPositionDelta ();
+
+    private:
+        baci::SmartPropertyPointer<baci::RWdouble> m_currentPositionAlpha_sp;
+        baci::SmartPropertyPointer<baci::RWdouble> m_currentPositionDelta_sp;
+};
+
+#endif
